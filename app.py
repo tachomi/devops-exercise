a9f078099621364fde9d3ad38bbf2cd18f93ae69
EC2 Default User <ec2-user@ip-172-31-17-88.ec2.internal>
2019-12-17 23:09:38 +0000

    adding words

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, I've changed!!!"
+    return "Hello World, I've changed. And I've changed again!!!"
 
 
 if __name__ == "__main__":
